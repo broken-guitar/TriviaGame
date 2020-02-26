732fd3ed66f6faa6009cedbceb251bc0c14593e7
DFlores <drfloresj@gmail.com>
2020-02-25 18:12:56 -0800

    pseudo starter code; progress ~5%

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+// VARIABLES
+
+var triviaQuestions = [{
+        question: "Who created Javascript?",
+        answers: {
+            a: {
+                text: "Microsoft",
+                correct: false
+            },
+            b: {
+                text: "Sun Microsystems",
+                correct: false
+            },
+            c: {
+                text: "Oracle",
+                correct: false
+            },
+            d: {
+                text: "Netscape",
+                correct: true
+            }
+        }
+    }
+
+];
+
+
+// FUNCTIONS
+
+//      timer function?
+//          if timer expired, then
+
+
+// INITIALIZE
+
+
+
+// TODO click on answer function
+
+//      start timer
+
+//      check if answer is correct
+
+//      if correct then
+//          update stuff, move to next question
+//      else
+//          update other stuff, move to next question?
