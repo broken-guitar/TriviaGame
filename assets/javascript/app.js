8fbeafc342f074a7273391e5ced34bc76f26db6d
DFlores <drfloresj@gmail.com>
2020-02-26 16:27:05 -0800

    adding trivia question objects, iteration code; progress ~20%

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,22 +1,25 @@
 // VARIABLES
 
-var triviaQuestions = [{
-        question: "Who created Javascript?",
+var $divQuestion = $("#question-container");
+var $divAnswers = $("#answer-container");
+
+var arrQuestions = [{
+        questionText: "Who created Javascript?",
         answers: {
             a: {
-                text: "Microsoft",
+                answerText: "Microsoft",
                 correct: false
             },
             b: {
-                text: "Sun Microsystems",
+                answerText: "Sun Microsystems",
                 correct: false
             },
             c: {
-                text: "Oracle",
+                answerText: "Oracle",
                 correct: false
             },
             d: {
-                text: "Netscape",
+                answerText: "Netscape",
                 correct: true
             }
         }
@@ -24,14 +27,56 @@ var triviaQuestions = [{
 
 ];
 
+var curQuestion = {};
+var arrHelper = [arrQuestions.length];
+arrHelper = arrHelper.fill(0, 0, arrQuestions.length);
+
 
 // FUNCTIONS
 
 //      timer function?
 //          if timer expired, then
 
+function displayNextQuestion() {
+    console.log(arrHelper[0]);
+    if (arrHelper.indexOf(0) >= 0) {
+        console.log("still some questions left...")
+        for (var i = 0; arrHelper.length; i++) {
+            if (arrHelper[i] == 1) {
+                console.log("skipped");
+                continue;
+            } else {
+                console.log("rending answers...");
+                arrHelper[i] = 1;
+                curQuestion = arrQuestions[i];
+                $divQuestion.append($("<h3>").text(curQuestion.questionText));
+                console.log("current question: ", curQuestion.questionText);
+                Object.entries(curQuestion.answers).forEach(([key, prop]) => {
+                    $divAnswers.append($("<h5>").attr("id", key).text(prop.answerText));
+                });
+                break;
+            }
+        }
+    } else {
+        console.log("no more questions")
+    }
+}
+
+
+
+
+
+
+
+
 
 // INITIALIZE
+
+arrHelper = arrHelper.fill(0, 0, arrQuestions.length);
+displayNextQuestion();
+
+
+
 
 
 
